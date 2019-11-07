6427bae1ae1dc082ed1bcbdca91cbbdc4afdfcd8
James Lee <jameswlee109@gmail.com>
2019-11-07 17:52:44 -0600

    working on reversedll

--- dll.js
+++ dll.js
@@ -50,6 +50,60 @@ class _Node {
         prev.next = newNode
         curr.prev = newNode
     }
+    insertAfter(newVal, item) {
+        let curr = this.head
+        while((curr !== null) && (curr.value !== item)){
+            curr = curr.next
+        }
+        let temp = curr.next
+        let newNode = new _Node(newVal, temp, curr)
+        curr.next = newNode
+        temp.prev = newNode
+    }
+    insertAt(pos, newVal) { // 5  10  15  20
+        if(this.head === null) {
+          throw new Error ('empty list')
+        }
+        let tracker = 0
+        let currNode = this.head
+        let prevNode = this.head
+        while(tracker !== pos - 1) {
+          prevNode = currNode
+          currNode = currNode.next
+          tracker++
+        }
+        let newNode = new _Node(newVal, currNode)
+        prevNode.next = newNode
+        currNode.prev = newNode
+      }
+    remove(item) {
+        if(this.head === null){
+            return null
+        }
+        if(this.head.value === item) {
+            this.head = this.head.next
+            return;
+        }
+        let curr = this.head
+        let prev = this.head
+        while((curr !== null) && (curr.value !== item)) {
+            prev = curr
+            curr = curr.next
+        }
+        let temp = curr.next
+        temp.prev = curr.prev
+        prev.next = curr.next
+    }
+    find(item) {
+        let curr = this.head
+        while(curr.value !== item) {
+            if(curr.next === null) {
+                console.log('not in list')
+            }
+            curr = curr.next
+        }
+        console.log(curr.value)
+    }
   }
   
   function display(ll) {
@@ -59,14 +113,33 @@ class _Node {
         curr = curr.next
     }
   }
+
+  function reverseDll(ll) { // 1 3 5 6 -> 6 5 3 1
+    let current = ll.head 
+    let prev = null
+    let next = ll.head.next 
+  
+    while(current !== null) {
+      next = current.next
+      current.next = prev
+      prev = current
+      current = next 
+  
+    }
+  
+    ll.head = prev
+    console.log(JSON.stringify(ll, null, 4))
+  }
   
   function main() {
     let dll = new DoubleLinkedList()
   
     dll.insertFirst('hi')
     dll.insertLast('bye')
-    dll.insertLast('bye')
-    dll.insertBefore('new', 'hi')
+    dll.insertLast('seeya')
+    dll.insertLast('new')
+    dll.insertLast('gahhhh')
+    dll.find('asdas')
     display(dll)
   
   }
