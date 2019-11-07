1e4fefb877b9f342f863144c5d465a64d0c78d8d
nphiukhao <nphiukhao@gmail.com>
2019-11-07 15:01:07 -0800

    dll insert before attempt

--- dll.js
+++ dll.js
@@ -9,7 +9,7 @@ class _Node {
 class DoubleLinkedList {
     constructor() {
         this.head = null
-        this.tail = null
+        // this.tail = null
     }
     insertFirst(item) {
         this.head = new _Node(item, this.head, null)
@@ -18,47 +18,63 @@ class DoubleLinkedList {
     }
 
     insertLast(item) {
-        if(this.head === null) {
+        if (this.head === null) {
             this.insertFirst(item)
         }
         else {
             let tempNode = this.head
-            while(tempNode.next !== null) {
+            while (tempNode.next !== null) {
                 tempNode = tempNode.next
             }
             let newNode = new _Node(item, null, tempNode)
             tempNode.next = newNode
-            this.tail = newNode
+            //this.tail = newNode
+            //console.log(this.tail.next)
 
         }
     }
-    // insertBefore(newVal, item){
-    //     if(this.head === null) {
-    //         console.log('list is empty') 
-    //     }
+    insertBefore(newVal, item) {
+        if (this.head === null) {
+            console.log('list is empty')
+        }
 
-    //     let tempNode = this.head
-    //     let prevNode = this.head
-    //     while((tempNode.value !== item) && (tempNode !== null)) {
-    //         prevNode = tempNode
-    //         tempNode = tempNode.next
-    //     }
-    //     let newItem = new _Node(newVal, tempNode, prevNode)
-    //     prevNode.next = newItem
-    //     tempNode.prev = newItem
-    // }
+        let tempNode = this.head
+        // let prevNode = this.head
+
+        // console.log(tempNode, prevNode)
+        // while((tempNode.value !== item) && (tempNode !== null)) {
+        //     prevNode = tempNode
+        //     tempNode = tempNode.next
+        //     console.log('loop')
+        // }
+
+        while (tempNode.next !== null) {
+            if (tempNode.next.value === item) {
+                let newItem = new _Node(newVal, tempNode.next, tempNode);
+                tempNode.next.prev =
+
+                return;
+            }
+            tempNode = tempNode.next;
+            // console.log('done')
+            // let newItem = new _Node(newVal, tempNode, prevNode)
+            // newItem.prev = tempNode.next.prev
+            // prevNode.next = newItem
+            // tempNode.prev = newItem
+        }
+    }
 
 }
 
 function display(ll) {
     let curr = ll.head
-    while(curr !== null) {
-      console.log(curr.value)
-      curr = curr.next
+    while (curr !== null) {
+        console.log(curr.value)
+        curr = curr.next
     }
-  }
+}
 
-function main(){
+function main() {
     let dll = new DoubleLinkedList()
 
     dll.insertFirst('hi')
